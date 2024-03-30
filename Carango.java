0e9ea2932bf3668432741091464fdc08c259ffa2
João <joao_gabriel.silva15@hotmail.com>
2024-03-30 16:01:51 -0300

    commit 16

---
+++ Carango.java
@@ -0,0 +1,19 @@
+public class Carango {
+
+    private int ano;
+
+    public Carango(){
+        
+    }
+    public Carango(int ano){
+        
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+}
--- Program.java
+++ Program.java
@@ -30,7 +30,10 @@ public class Program{
        // Ex10 ex10 = new Ex10();
        // ex10.executar();
 
-       Ex11 ex11 = new Ex11();
-       ex11.executar();
+       // Ex11 ex11 = new Ex11();
+       // ex11.executar();
+
+       Ex12 ex12 = new Ex12();
+       ex12.executar();
     }
 }
