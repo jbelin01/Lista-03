a18b5126f2d1c13e26fa174b3940e5c35bb80911
João <joao_gabriel.silva15@hotmail.com>
2024-03-31 14:08:33 -0300

    commit 25

---
+++ Ex20.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Ex20 {
+    
+    public void executar() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o número de horas trabalhadas:");
+        int horasTrabalhadas = scanner.nextInt();
+
+        System.out.println("Digite o nível do professor (1, 2 ou 3):");
+        int nivel = scanner.nextInt();
+
+        Professor professor = new Professor();
+        professor.setHorasTrabalhadas(horasTrabalhadas);
+        professor.setNivel(nivel);
+
+        double salario = professor.calcularSalario();
+        System.out.println("O salário do professor é: R$" + salario);
+
+        scanner.close();
+    }
+
+    public static void main(String[] args) {
+        Ex20 programa = new Ex20();
+        programa.executar();
+    }
+}
---
+++ Triangulo.java
@@ -0,0 +1,25 @@
+class Triangulo {
+    private double lado1;
+    private double lado2;
+    private double lado3;
+    
+    public Triangulo(double lado1, double lado2, double lado3) {
+        this.lado1 = lado1;
+        this.lado2 = lado2;
+        this.lado3 = lado3;
+    }
+    
+    public boolean ehTriangulo() {
+        return lado1 + lado2 > lado3 && lado1 + lado3 > lado2 && lado2 + lado3 > lado1;
+    }
+    
+    public String tipoTriangulo() {
+        if (lado1 == lado2 && lado2 == lado3) {
+            return "Equilátero";
+        } else if (lado1 == lado2 || lado1 == lado3 || lado2 == lado3) {
+            return "Isóscele";
+        } else {
+            return "Escaleno";
+        }
+    }
+}
